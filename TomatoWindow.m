d8ed194ddd2e0e4e818a5cb01c814e4b5426ad9f
Scott Barron <scott@elitists.net>
2009-03-27 14:14:54 -0400

    don't want this level

--- TomatoWindow.m
+++ TomatoWindow.m
@@ -13,7 +13,6 @@
 - (id)initWithContentRect:(NSRect)contentRect styleMask:(unsigned int)aStyle backing:(NSBackingStoreType)bufferingType defer:(BOOL)flag {
     NSWindow *result = [super initWithContentRect:contentRect styleMask:NSBorderlessWindowMask backing:NSBackingStoreBuffered defer:NO];
     [result setBackgroundColor:[NSColor clearColor]];
-    [result setLevel:NSStatusWindowLevel];
     [result setAlphaValue:1.0];
     [result setOpaque:NO];
     [result setHasShadow:YES];
